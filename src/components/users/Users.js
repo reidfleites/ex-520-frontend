829f26571005eb6aa5bf7bb95c1f018bf579fc0f
reidfleites <dreinier86@gmail.com>
2022-01-04 12:55:54 +0100

    ok

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import {  useEffect,useEffect } from "react";
+import {  useState,useEffect } from "react";
 
 import {FaUserAlt} from "react-icons/fa";
 import {MdEmail} from "react-icons/md";
